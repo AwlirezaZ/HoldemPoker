298af0351c7ce4a3ca11795475d0fa2db62b6e4f
Alireza Ahmadi <ahmadialireza317@gmail.com>
2022-01-04 19:16:55 +0330

    add domain library and add player class

---
+++ Domain/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Player
+    {
+        public string PlayerName { get; set; }
+        public Player(string playerName)
+        {
+            PlayerName = playerName;
+        }
+    }
+}
